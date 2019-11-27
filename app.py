bc37d717180d8446b719d8495216fb9fe6dd22df
Kevin Quach <kevin.quach@outlook.fr>
2019-11-27 09:51:51 +0100

    lanch server

--- app.py
+++ app.py
@@ -35,3 +35,6 @@ def dump_to_csv(d):
 	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
 		writer = csv.writer(f)
 		writer.writerow(donnees)
+
+if __name__ == "__main__":
+    app.run()
