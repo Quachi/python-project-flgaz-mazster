4f169b5ac3c756b00b61754816455f9e49f24cb1
Kevin Quach <kevin.quach@outlook.fr>
2019-11-27 16:53:26 +0100

    test connection

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for
 import csv
+from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'kquach.mysql.eu.pythonanywhere-services.com'
+app.config['MYSQL_DATABASE_USER'] = 'kquach'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'azerty94'
+app.config['MYSQL_DATABASE_DB'] = 'flgaz'
+mysql.init_app(app)
 
 
 @app.route('/')
@@ -24,6 +32,28 @@ def save_gazouille():
 def timeline():
     gaz = parse_from_csv()
     return render_template("timeline.html", gaz=gaz)
+
+
+@app.route('/twitt', methods=['GET'])
+def get_twitt():
+    if request.method == 'POST':
+        print(request.form)
+        dump_to_csv(request.form)
+        return redirect(url_for('timeline'))
+        # return "OK"
+    if request.method == 'GET':
+        return render_template('formulaire.html')
+
+
+@app.route('/test')
+def someName():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * from Message")
+    data = cursor.fetchone()
+    print(data)
+    return render_template('formulaire.html')
 
 
 def parse_from_csv():
