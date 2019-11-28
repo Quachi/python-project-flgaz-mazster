49515bbe1ef0cfcd2cc2c6de0eae0156c75f165b
Kevin Quach <kevin.quach@outlook.fr>
2019-11-28 12:57:40 +0100

    update message

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def dump_to_csv(d):
 
 
 class Message(db.Model):
-    __tablename__ = 'Message'
+    __tablename__ = 'message'
     id = db.Column(db.Integer,
                    primary_key=True)
     name = db.Column(db.String(64),
