e850da06b0426f469e48b05fdb8089ade9b64374
Kevin Quach <kevin.quach@outlook.fr>
2019-11-29 10:17:56 +0100

    fix piplint

--- app.py
+++ app.py
@@ -1,96 +1,139 @@
-from flask import Flask, request, render_template, redirect, url_for
+"""
+      file app: entry point for project gaz
+"""
 import csv
-from flask import json
+from flask import Flask, request, render_template, redirect, url_for
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-if app.config["ENV"] == "production":
-    app.config.from_object("config.ProductionConfig")
+if APP.config["ENV"] == "production":
+    APP.config.from_object("config.ProductionConfig")
 else:
-    app.config.from_object("config.DevelopmentConfig")
+    APP.config.from_object("config.DevelopmentConfig")
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
-app.config['CORS_HEADERS'] = 'Content-Type'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+APP.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
+APP.config['CORS_HEADERS'] = 'Content-Type'
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 
-@app.route('/')
+@APP.route('/')
 def home():
+    """
+   function home: show route index
+   """
     return 'Bienvenue !'
 
 
-@app.route('/gaz', methods=['GET', 'POST'])
+@APP.route('/gaz', methods=['GET', 'POST'])
 def save_gazouille():
+    """
+      function save_gazouille: save gazouille or show form to add a gaz
+    """
     if request.method == 'POST':
         print(request.form)
-        addMessage(request.form)
+        add_message(request.form)
         return redirect(url_for('timeline'))
         # return "OK"
     if request.method == 'GET':
         return render_template('formulaire.html')
 
 
-@app.route('/timeline', methods=['GET'])
+@APP.route('/timeline', methods=['GET'])
 def timeline():
-    messages = getMessage()
+    """
+      function timeline: show all gazouille from oldest to latest
+      Return
+      -------
+      template
+      render html template
+    """
+    messages = get_message()
     return render_template("timeline.html", messages=messages)
 
 
-@app.route('/timeline/<username>', methods=['GET'])
-def timelineUser(username):
+@APP.route('/timeline/<username>', methods=['GET'])
+def timeline_user(username):
+    """
+      function timeline: show all gazouille to one user
+      Return
+      -------
+      template
+      render html template timeline
+    """
     messages = Message.query.filter_by(name=username)
     return render_template("timeline.html", messages=messages)
 
 
 def parse_from_csv():
+    """
+      function timeline: read and return all data in gazouille
+      Return
+      -------
+      array gaz
+    """
     gaz = []
-    with open('./gazouilles.csv', 'r') as f:
-        reader = csv.reader(f)
+    with open('./gazouilles.csv', 'r') as file:
+        reader = csv.reader(file)
         for row in reader:
             gaz.append({"user": row[0], "text": row[1]})
     return gaz
 
 
-def dump_to_csv(d):
-    donnees = [d["user-name"], d["user-text"]]
-    with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
+def dump_to_csv(data):
+    """
+      function timeline: add data in gazouille csv
+    """
+    donnees = [data["user-name"], data["user-text"]]
+    with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
         writer.writerow(donnees)
 
 
-def addMessage(d):
-    donnees = [d["user-name"], d["user-text"]]
+def add_message(data):
+    """
+      function timeline: add message in database
+    """
     message = Message(
-        name=d["user-name"],
-        text=d["user-text"]
+        name=data["user-name"],
+        text=data["user-text"]
     )
-    db.session.add(message)
-    db.session.commit()
+    DB.session.add(message)
+    DB.session.commit()
 
 
-def getMessage():
+def get_message():
+    """
+      function timeline: get all message in database
+      Return
+      -------
+      messages
+    """
     messages = Message.query.all()
     return messages
 
 
-class Message(db.Model):
+class Message(DB.Model):
+    """
+      class Message: DTO of message
+    """
     __tablename__ = 'message'
-    id = db.Column(db.Integer,
-                   primary_key=True)
-    name = db.Column(db.String(64),
-                     index=False,
-                     nullable=False)
-    text = db.Column(db.String(280),
-                     index=True,
-                     nullable=False)
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64),
+                  index=False,
+                  nullable=False)
+    text = Column(String(280),
+                  index=True,
+                  nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
 
-db.create_all()
+DB.create_all()
